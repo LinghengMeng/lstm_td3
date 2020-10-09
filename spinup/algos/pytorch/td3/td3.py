780e22a43a3654f42c90a5786a24d3837b264fbe
Lingheng Meng <linghengmeng@yahoo.com>
2020-10-08 22:43:14 -0400

    t

--- spinup/algos/pytorch/td3/td3.py
+++ spinup/algos/pytorch/td3/td3.py
@@ -375,6 +375,7 @@ if __name__ == '__main__':
     data_dir = osp.join(
         osp.dirname(osp.dirname(osp.dirname(osp.dirname(osp.dirname(osp.dirname(osp.abspath(__file__))))))),
         args.data_dir)
+    print('data_dir={}'.format(data_dir))
     logger_kwargs = setup_logger_kwargs(args.exp_name, args.seed, data_dir, datestamp=True)
 
     td3(env_name=args.env, partially_observable=args.partially_observable,
