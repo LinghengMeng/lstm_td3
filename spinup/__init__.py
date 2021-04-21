6f7ba358da3a9832ccd7cca2d68ff33f0e311a3a
Lingheng <linghengmeng@yahoo.com>
2021-04-21 16:39:12 -0400

    comment out import in __init__.py

--- spinup/__init__.py
+++ spinup/__init__.py
@@ -4,19 +4,19 @@ import tensorflow as tf
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 # Algorithms
-from spinup.algos.tf1.ddpg.ddpg import ddpg as ddpg_tf1
-from spinup.algos.tf1.ppo.ppo import ppo as ppo_tf1
-from spinup.algos.tf1.sac.sac import sac as sac_tf1
-from spinup.algos.tf1.td3.td3 import td3 as td3_tf1
-from spinup.algos.tf1.trpo.trpo import trpo as trpo_tf1
-from spinup.algos.tf1.vpg.vpg import vpg as vpg_tf1
+# from spinup.algos.tf1.ddpg.ddpg import ddpg as ddpg_tf1
+# from spinup.algos.tf1.ppo.ppo import ppo as ppo_tf1
+# from spinup.algos.tf1.sac.sac import sac as sac_tf1
+# from spinup.algos.tf1.td3.td3 import td3 as td3_tf1
+# from spinup.algos.tf1.trpo.trpo import trpo as trpo_tf1
+# from spinup.algos.tf1.vpg.vpg import vpg as vpg_tf1
 
-from spinup.algos.pytorch.ddpg.ddpg import ddpg as ddpg_pytorch
-from spinup.algos.pytorch.ppo.ppo import ppo as ppo_pytorch
-from spinup.algos.pytorch.sac.sac import sac as sac_pytorch
-from spinup.algos.pytorch.td3.td3 import td3 as td3_pytorch
-from spinup.algos.pytorch.trpo.trpo import trpo as trpo_pytorch
-from spinup.algos.pytorch.vpg.vpg import vpg as vpg_pytorch
+# from spinup.algos.pytorch.ddpg.ddpg import ddpg as ddpg_pytorch
+# from spinup.algos.pytorch.ppo.ppo import ppo as ppo_pytorch
+# from spinup.algos.pytorch.sac.sac import sac as sac_pytorch
+# from spinup.algos.pytorch.td3.td3 import td3 as td3_pytorch
+# from spinup.algos.pytorch.trpo.trpo import trpo as trpo_pytorch
+# from spinup.algos.pytorch.vpg.vpg import vpg as vpg_pytorch
 
 # Loggers
 from spinup.utils.logx import Logger, EpochLogger
