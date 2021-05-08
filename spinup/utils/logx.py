949b0733290a098f5c090797cc9651c8c3055d6d
Lingheng <linghengmeng@yahoo.com>
2021-05-08 00:02:22 -0400

    add non-mpi tools

--- spinup/utils/logx.py
+++ spinup/utils/logx.py
@@ -14,6 +14,7 @@ import tensorflow as tf
 import torch
 import os.path as osp, time, atexit, os
 import warnings
+from spinup.utils.tools import statistics_scalar
 from spinup.utils.serialization_utils import convert_json
 from spinup.user_config import DEFAULT_DATA_DIR, FORCE_DATESTAMP, \
                                DEFAULT_SHORTHAND, WAIT_BEFORE_LAUNCH
@@ -71,29 +72,29 @@ def restore_tf_graph(sess, fpath):
     return model
 
 
-def statistics_scalar(x, with_min_and_max=False):
-    """
-    Get mean/std and optional min/max of scalar x across MPI processes.
-
-    Args:
-        x: An array containing samples of the scalar to produce statistics
-            for.
-
-        with_min_and_max (bool): If true, return min and max of x in
-            addition to mean and std.
-    """
-    x = np.array(x, dtype=np.float32)
-    global_sum, global_n = x.sum(), len(x)
-    mean = global_sum / global_n
-
-    global_sum_sq = np.sum((x - mean)**2)
-    std = np.sqrt(global_sum_sq / global_n)  # compute global std
-
-    if with_min_and_max:
-        global_min = np.min(x) if len(x) > 0 else np.inf
-        global_max = np.max(x) if len(x) > 0 else -np.inf
-        return mean, std, global_min, global_max
-    return mean, std
+# def statistics_scalar(x, with_min_and_max=False):
+#     """
+#     Get mean/std and optional min/max of scalar x across MPI processes.
+#
+#     Args:
+#         x: An array containing samples of the scalar to produce statistics
+#             for.
+#
+#         with_min_and_max (bool): If true, return min and max of x in
+#             addition to mean and std.
+#     """
+#     x = np.array(x, dtype=np.float32)
+#     global_sum, global_n = x.sum(), len(x)
+#     mean = global_sum / global_n
+#
+#     global_sum_sq = np.sum((x - mean)**2)
+#     std = np.sqrt(global_sum_sq / global_n)  # compute global std
+#
+#     if with_min_and_max:
+#         global_min = np.min(x) if len(x) > 0 else np.inf
+#         global_max = np.max(x) if len(x) > 0 else -np.inf
+#         return mean, std, global_min, global_max
+#     return mean, std
 
 
 def setup_logger_kwargs(exp_name, seed=None, data_dir=None, datestamp=False):
